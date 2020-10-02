36e4f52fd5b78810cb8d694d36d04787a147ff12
Bruno Alves <vespidhook@gmail.com>
2020-10-02 13:56:38 -0300

    finalizado

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# netflix-clone
-Clone do NETFLIX em REACTJS
+<p align="center">
+  <img  width="300" src="https://www.abacustechnologies.com/wp-content/uploads/2020/05/580b57fcd9996e24bc43c529-1536x864.png">
+  <img  width="200" src="https://b7web.com.br/wp-content/uploads/2018/12/b7_logo.png">
+</p>
+
+# Netflix Clone com a B7Web
+
+Este projeto é um clone simplificado do front-end do Netflix. Foi criado com React e CSS. Ele usa [The MovieDB API](https://www.themoviedb.org/documentation/api) para pesquisar filmes e exibir detalhes. Sinta-se à vontade para contribuir!
+
+### Executando o projeto localmente
+
+- Instale dependências: execute `npm install` no projeto raiz
+- Obtenha a chave da API [aqui](https://www.themoviedb.org/documentation/api)
+- Execute o projeto: `npm run dev`
+
+### O projeto:
+
+- O usuário pode pesquisar filmes e programas de TV no TMDb
+- O usuário pode ver os próximos filmes e tendências. Atualizações de dados semanais
+
+- A página da web se adapta a qualquer tamanho de tela.
+
+Sinta-se à vontade para criar uma solicitação pull e enviar quaisquer problemas!
---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
+
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o Featured
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      setFeaturedData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+
+  return (
+    <div className="page">
+      <Header black={blackHeader} />
+
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Feito com{" "}
+        <span role="img" aria-label="coração">
+          ❤️
+        </span>{" "}
+        por Bruno Alves e B7Web
+        <br />
+        Direitos de imagem para Netflix
+        <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif"
+            alt="Carregando"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
